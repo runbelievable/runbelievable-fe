b9c604208c0f2ff6352607cc726ed4fd680d0ff3
Mandy Alford <mandyalford@gmail.com>
2020-05-27 21:24:15 -0600

    -m 'Build out login component and basic state access functionality'
    Co-authored-by: Tanisha L Davey <tanisha.davey2@gmail.com>

--- components/Login.js
+++ components/Login.js
@@ -5,30 +5,67 @@ import { createStackNavigator } from '@react-navigation/stack';
 import CustomButton from './CustomButton';
 
 export default class Login extends Component {
-  constructor({navigation}){
-    super({navigation});
-    this.state = {};
-  }
+    state = {
+      username: '',
+      password: '',
+    }
+
+
+
+    // divb.addEventListener('click', function(e) {
+    //   savedEvent = e;
+    //   savedTarget = e.currentTarget;
+    //   setTimeout(function() {
+    //     console.log('b: currentTarget is now ' + e.currentTarget);
+    //   }, 0);
+    // }, false);
+
+
+  // updateUserName = (event) => {
+  //   var savedEvent;
+  //   var savedTarget;
+  //
+  //   savedEvent = event;
+  //   savedTarget = event.currentValue;
+  //   // event.preventDefault();
+  //   // console.log(savedEvent)
+  //   console.log(savedTarget)
+  //   // let value= event.target && event.target.value
+  //   this.setState({username: event.target}, () => {console.log(this.state)})
+  // }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.instructions}>Enter your username and password:</Text>
         <TextInput
         type='email'
+        name='username'
         value={this.state.username}
         style={{borderColor: 'grey', borderWidth: 1, height: 50, width: 250, marginBottom: 15, paddingLeft: 10}}
         placeholder='username'
+        onChange={(e) => {
+          this.setState({username: e.nativeEvent.text})
+        }}
         />
         <TextInput
         type='password'
-        value={this.state.password}
+        name='password'
+        onChange={(e) => {
+          this.setState({password: e.nativeEvent.text})
+        }}
+        // value={this.state.password}
         style={{borderColor: 'grey', borderWidth: 1, height: 50, width: 250, marginBottom: 15, paddingLeft: 10}}
         placeholder='password'
         />
         <CustomButton
         title='Submit'
         color='green'
-        accesibilityLabel='Submit your login information'/>
+        accesibilityLabel='Submit your login information'
+        onPress={(e) => {
+          console.log(this.state)
+        }}
+        />
       </View>
     )
   }
