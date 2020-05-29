1c271c64b6a74c6759e1366a13cffe92751c87e5
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-05-29 11:29:52 -0600

    -m "Add apiCall for creating a new user"
    Co-authored-by: Mandy Alford <mandy.alford@gmail.com>

--- components/apiCalls.js
+++ components/apiCalls.js
@@ -7,3 +7,21 @@ export const getSpecificUser = (id) => {
   fetch(`https://run-be.herokuapp.com/api/v1/${id}`)
     .then(response => response.json())
 }
+
+export const createUser = () => {
+  fetch('https://run-be.herokuapp.com/api/v1/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'appplication/json'
+    },
+    body: JSON.stringify({
+      'first_name': firstName,
+      'last_name': lastName,
+      'age': age,
+      'gender': gender,
+      'max_run_distance': maxRunDistance,
+      'estimated_mile_pace': estimatedMilePlace
+    })
+  })
+    .then(response => response.json())
+}
