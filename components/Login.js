4f4f663786c0aeb7b1cdf58706272d1eac9bb107
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-05-31 11:34:40 -0600

    -m "Add error message to display when inputs are empty on click of submit"
    Co-authored-by: Mandy Alford <mandyalford@gmail.com>

--- components/Login.js
+++ components/Login.js
@@ -3,26 +3,32 @@ import { StyleSheet, Text, Button, View, TextInput, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import CustomButton from './CustomButton';
+import { validateUser } from '../apiCalls';
 
 export default class Login extends Component {
     state = {
       username: '',
       password: '',
+      error: ''
     }
 
-
   submitUserCredentials(props){
-    this.validateUserInfo()
-    this.setState({ username: '', password: '' })
+    if (this.state.username === '' || this.state.password === '') {
+      this.setState({ error: 'Please enter a valid username and password.'})
+    } else {
+      this.validateUserInfo()
+      this.setState({ username: '', password: '' })
+    }
   }
 
   validateUserInfo(props) {
-    fetch(`https://run-be.herokuapp.com/api/v1/login?username=${this.state.username}&password=${this.state.password}`)
+      validateUser(this.state.username, this.state.password)
       .then(response => {
         if (response.ok) {
+          console.log('a ok');
           this.props.navigation.navigate('User')
         } else {
-          Alert.alert('Please enter a valid username and password.')
+          Alert.alert('no')
         }
       })
       .catch(error => console.log(error))
@@ -30,40 +36,55 @@ export default class Login extends Component {
 
 
   render() {
-    console.log(this.props.navigation)
-    return (
-      <View style={styles.container}>
-        <Text style={styles.instructions}>Enter your username and password:</Text>
-        <TextInput
-        type='email'
-        name='username'
-        value={this.state.username}
-        style={{borderColor: 'grey', borderWidth: 1, height: 50, width: 250, marginBottom: 15, paddingLeft: 10}}
-        placeholder='username'
-        onChange={(e) => {
-          this.setState({username: e.nativeEvent.text})
-        }}
-        />
-        <TextInput
-        type='password'
-        name='password'
-        onChange={(e) => {
-          this.setState({password: e.nativeEvent.text})
-        }}
-        value={this.state.password}
-        style={{borderColor: 'grey', borderWidth: 1, height: 50, width: 250, marginBottom: 15, paddingLeft: 10}}
-        placeholder='password'
-        />
-        <CustomButton
-        title='Submit'
-        color='green'
-        accesibilityLabel='Submit your login information'
-        onPress={(props) => this.submitUserCredentials(props)}
-        />
-      </View>
-    )
+    let pageView =
+    <View>
+    <Text style={styles.instructions}>Enter your username and password:</Text>
+    <TextInput
+    type='email'
+    name='username'
+    value={this.state.username}
+    style={{borderColor: 'grey', borderWidth: 1, height: 50, width: 250, marginBottom: 15, paddingLeft: 10}}
+    placeholder='username'
+    onChange={(e) => {
+      this.setState({username: e.nativeEvent.text, error: ''})
+    }}
+    />
+    <TextInput
+    type='password'
+    name='password'
+    onChange={(e) => {
+      this.setState({password: e.nativeEvent.text, error: ''})
+    }}
+    value={this.state.password}
+    style={{borderColor: 'grey', borderWidth: 1, height: 50, width: 250, marginBottom: 15, paddingLeft: 10}}
+    placeholder='password'
+    />
+    <CustomButton
+    title='Submit'
+    color='green'
+    accesibilityLabel='Submit your login information'
+    onPress={(props) => this.submitUserCredentials(props)}
+    />
+    </View>
+
+      if (this.state.error) {
+        return (
+          <View style={styles.container}>
+            <Text>{this.state.error}</Text>
+            {pageView}
+          </View>
+        )
+      } else {
+        return (
+          <View style={styles.container}>
+            {pageView}
+          </View>
+        )
+      }
+
+    }
   }
-}
+
 
 const styles = StyleSheet.create({
   container: {
