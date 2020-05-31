25c9738d33ef668d6a17ef81ea96590e53e613bd
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-05-31 16:25:39 -0600

    -m "Update return to use route props for user information"
    Co-authored-by: Mandy Alford <mandyalford@gmail.com>

--- components/User.js
+++ components/User.js
@@ -1,29 +1,25 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Header from './Header';
 import { getSingleUser } from '../apiCalls';
 
 export default class User extends Component {
-  state: {
-    userInfo: {}
-  }
 
   componentDidMount() {
-    console.log(this.props.route.params.userInfo)
-    getSingleUser(this.props.route.params.userInfo)
-      .then(user => this.setState({ userInfo: user.data}))
+    getSingleUser(this.props.route.params.userInfo.id)
+      .then(user => this.setState({ userInfo: user.data }))
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Header navigation={this.props.navigation}/>
-        <Text>{`${this.state.userInfo.data.attributes.first_name} ${this.state.userInfo.data.attributes.last_name}`}</Text>
-        <Text>Favorite Routes</Text>
-        <Text>Pace: {this.state.userInfo.data.attributes.estimated_mile_pace}/ min</Text>
-        <Text>Location: {this.state.userInfo.data.attributes.location}</Text>}
+        <Text>{this.props.route.params.userInfo.attributes.first_name}</Text>
+          <Text>Favorite Routes</Text>
+        <Text>Pace: {this.props.route.params.userInfo.attributes.estimated_mile_pace}/ min</Text>
+        <Text>Location: {this.props.route.params.userInfo.attributes.location}</Text>
       </View>
     )
   }
