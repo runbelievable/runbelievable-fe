d871b8bae0dc069b092d4cab1481a0eae881d7d6
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-05-31 16:02:05 -0600

    -m "Add fetch call to get a single user"
    Co-authored-by: Mandy Alford <mandyalford@gmail.com>

--- apiCalls.js
+++ apiCalls.js
@@ -6,3 +6,8 @@ export const getBuddies = () => {
 export const validateUser = (name, pword) => {
   return fetch(`https://run-be.herokuapp.com/api/v1/login?username=${name}&password=${pword}`)
 }
+
+export const getSingleUser = (id) => {
+  return fetch(`https://run-be.herokuapp.com/api/v1/users/${id}`)
+    .then(response => response.json())
+}
