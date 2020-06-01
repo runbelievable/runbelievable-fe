83b2d6c07f233b8bfe36ac7743a807d2d495a601
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-06-01 17:36:29 -0600

    -m "Add button to find another random trail"
    Co-authored-by: Mandy Alford <mandyalford@gmail.com>

--- components/Trail.js
+++ components/Trail.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, Linking } from 'react-native';
+import { View, StyleSheet, Text, Linking, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Header from './Header';
+import { findRandomTrail } from '../apiCalls';
 
 
-export default function App() {
+export default function Trail() {
+
+  const getNewTrail = () => {
+    findRandomTrail()
+  }
 
   return (
-    <View>
+    <View style={styles.container}>
       <Header />
+      <Text style={styles.heading}>Here's a trail you might like!</Text>
       <Text>Trail Name</Text>
       <Text>Summary</Text>
       <Text>Location</Text>
@@ -19,7 +25,21 @@ export default function App() {
       onPress={() => Linking.openURL('http://google.com')}>
       http://google.com
       </Text>
+      <Button
+        title='Try another trail'
+        color='darkorange'
+        onPress={() => getNewTrail() }
+      />
     </View>
   )
-
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  heading: {
+    fontSize: 24,
+    marginBottom: 15
+  }
+})
