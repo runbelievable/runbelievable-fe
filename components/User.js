7b3c342826b4531ffd9778d665eb37fe1a13045b
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-06-02 09:55:09 -0600

    -m "Change component to functional component"
    Co-authored-by: Mandy Alford <mandyalford@gmail.com>

--- components/User.js
+++ components/User.js
@@ -1,29 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View, Image, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Header from './Header';
 import { getSingleUser } from '../apiCalls';
 
-export default class User extends Component {
+export default function User({ navigation, route }) {
 
-  componentDidMount() {
-    getSingleUser(this.props.route.params.userInfo.id)
-      .then(user => this.setState({ userInfo: user.data }))
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Header navigation={this.props.navigation}/>
-        <Text>{this.props.route.params.userInfo.attributes.first_name}</Text>
-          <Text>Favorite Routes</Text>
-        <Text>Pace: {this.props.route.params.userInfo.attributes.estimated_mile_pace}/ min</Text>
-        <Text>Location: {this.props.route.params.userInfo.attributes.location}</Text>
-      </View>
-    )
-  }
-
+  return (
+    <View style={styles.container}>
+      <Header navigation={navigation}/>
+      <Text>{route.params.userInfo.attributes.first_name}</Text>
+        <Text>Favorite Routes</Text>
+      <Text>Pace: {route.params.userInfo.attributes.estimated_mile_pace}/ min</Text>
+      <Text>Location: {route.params.userInfo.attributes.location}</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
