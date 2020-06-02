4c0c85596a448ded40a2731887afba5a29441e71
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-06-02 09:06:30 -0600

    -m "Add activityIndicator and change component to a class"
    Co-authored-by: Mandy Alford <mandyalford@gmail.com>

--- components/Trail.js
+++ components/Trail.js
@@ -1,37 +1,51 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, Linking, Button } from 'react-native';
+import { View, StyleSheet, Text, Linking, Button, ActivityIndicator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Header from './Header';
 import { findRandomTrail } from '../apiCalls';
 
 
-export default function Trail() {
+export default class Trail extends Component {
+  state = {}
 
-  const getNewTrail = () => {
+  componentDidMount() {
     findRandomTrail()
+      .then(data => console.log(data))
   }
 
-  return (
-    <View style={styles.container}>
-      <Header />
-      <Text style={styles.heading}>Here's a trail you might like!</Text>
-      <Text>Trail Name</Text>
-      <Text>Summary</Text>
-      <Text>Location</Text>
-      <Text>Length</Text>
-      <Text
-      style={{color: 'blue'}}
-      onPress={() => Linking.openURL('http://google.com')}>
-      http://google.com
-      </Text>
-      <Button
-        title='Try another trail'
-        color='darkorange'
-        onPress={() => getNewTrail() }
-      />
-    </View>
-  )
+  render() {
+    if (state = {}) {
+      return (
+        <View style={styles.activityIndicator}>
+          <ActivityIndicator
+            size='large'
+            color='darkorange' />
+        </View>
+      )
+    } else {
+      return (
+        <View style={styles.container}>
+          <Header />
+          <Text style={styles.heading}>Here's a trail you might like!</Text>
+          <Text>Trail Name</Text>
+          <Text>Summary</Text>
+          <Text>Location</Text>
+          <Text>Length</Text>
+          <Text
+          style={{color: 'blue'}}
+          onPress={() => Linking.openURL('http://google.com')}>
+          http://google.com
+          </Text>
+          <Button
+            title='Try another trail'
+            color='darkorange'
+            onPress={() => getNewTrail() }
+          />
+        </View>
+      )
+    }
+  }
 }
 
 const styles = StyleSheet.create({
@@ -41,5 +55,8 @@ const styles = StyleSheet.create({
   heading: {
     fontSize: 24,
     marginBottom: 15
+  },
+  activityIndicator: {
+    marginTop: 50
   }
 })
